3c0f603fff7e30751977c6c48cc49c56e2b43abe
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2024-04-29 18:22:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/react-vite-realworld-example-app',
-  server: {
-    host: true,
-  },
+  plugins: [reactRefresh()],
+  build: {
+    outDir: '/src/main.jsx', // Specify the output directory for the build
+    emptyOutDir: true // Clear the output directory before building
+  }
 });
